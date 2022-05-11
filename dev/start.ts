51a9e2596dc6076d3828df544e8ab3e48b411800
pleahmacaka <kkjuh0905@gmail.com>
2022-05-11 22:34:09 +0900

    Feat: dev bot

---
+++ dev/start.ts
@@ -0,0 +1,21 @@
+import { Client } from "../package/Client"
+import { ALL_INTENTS } from "../package/util/AllIntents"
+import { example } from "./commands/example"
+
+const config = require("./config.json")
+
+const client = new Client(
+	{
+		intents: ALL_INTENTS
+	}, true
+)
+
+client.addSlash(example)
+
+client.once("ready", () => {
+	console.log(`${client.user.username} is ready!`)
+})
+
+client.login(config.TOKEN).then(token => {
+	client.deploy(token)
+})
