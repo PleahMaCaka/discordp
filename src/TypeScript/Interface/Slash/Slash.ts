314f59c6bd95424647e8c6c3fff83edcd0b49958
SEOA7777 <50793769+PleahMaCaka@users.noreply.github.com>
2022-05-27 23:45:27 +0900

    Refactor: missed fix

--- src/TypeScript/Interface/Slash/Slash.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInteraction } from "discord.js";
-import { Client } from "../../../Client";
-
-export interface Slash {
-	name: string
-	description?: string
-
-	execute(interaction: CommandInteraction, client?: Client): any
-}
