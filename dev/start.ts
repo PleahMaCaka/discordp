46a083fa27a10f608d5d39af48c58fcb8bc4dea0
PleahMaCaka <kkjuh0905@gmail.com>
2022-05-29 20:31:57 +0900

    :up: DevBot Upgrade

--- dev/start.ts
+++ dev/start.ts
@@ -1,21 +1,34 @@
-import { Client } from "../package/Client"
-import { ALL_INTENTS } from "../package/util/AllIntents"
-import { example } from "./Commands/Commands"
+import { Client } from "../package/Client";
+import { ALL_INTENTS } from "../package/Utils/AllIntents";
+import { Slash } from "../package/TypeScript/Decorator/Slash";
+import { SlashClient } from "../package/commands/SlashClient";
+import { CommandInteraction } from "discord.js";
+
 
 const config = require("./config.json")
 
 const client = new Client(
 	{
 		intents: ALL_INTENTS
-	}, true
+	},
+	{
+		debug: true
+	}
 )
 
-client.addSlash(example)
+class ExampleCommand {
+	@Slash({ name: "example", description: "that is example slash command!" })
+	public async example(interaction: CommandInteraction) {
+		console.log("test example command is executed!")
+		await interaction.reply("stuff")
+	}
+
+}
 
 client.once("ready", () => {
-	console.log(`${client.user.username} is ready!`)
+	console.log(`${client.user!.username} is ready!`)
 })
 
 client.login(config.TOKEN).then(token => {
-	client.deploy(token)
+	SlashClient.deploy(token)
 })
