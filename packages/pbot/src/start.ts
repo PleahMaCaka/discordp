426e4af72db0ec9afdb19609365b48eb4bf9104a
pleahmacaka <pleahmacaka@gmail.com>
2023-01-13 22:26:48 +0900

    chore: devbot

--- packages/pbot/src/start.ts
+++ packages/pbot/src/start.ts
@@ -1,5 +1,32 @@
+import { GatewayIntentBits as Intent } from "discord-api-types/v10";
 import { Client } from "discordp";
 
 const client = new Client({
-    intents: []
+    intents: [
+        Intent.Guilds,
+        Intent.GuildMembers,
+        Intent.GuildBans,
+        Intent.GuildEmojisAndStickers,
+        Intent.GuildIntegrations,
+        Intent.GuildWebhooks,
+        Intent.GuildInvites,
+        Intent.GuildVoiceStates,
+        Intent.GuildPresences,
+        Intent.GuildMessages,
+        Intent.GuildMessageReactions,
+        Intent.GuildMessageTyping,
+        Intent.DirectMessages,
+        Intent.DirectMessageReactions,
+        Intent.DirectMessageTyping,
+        Intent.MessageContent,
+        Intent.GuildScheduledEvents,
+        Intent.AutoModerationConfiguration,
+        Intent.AutoModerationExecution,
+    ]
 })
+
+client.once("ready", () => {
+    console.log("Ready!")
+})
+
+client.login("TOKEN")
